fae9b4b26d935d37f550ba04ee4b736c6f30141c
Conor Patrick <conorpp94@gmail.com>
2020-11-18 21:27:45 +0100

    rename app/ to platform/lpc55/

--- app/run.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash -xe
-
-for i in {0..300}; do echo; done; \
-  cargo run --release \
-  --features log-semihosting,debug-trussed,debug-fido-authenticator,trussed-semihosting,fido-authenticator-semihosting,semihost-raw-responses \
-  --color always 2&>1 | less -r
-
