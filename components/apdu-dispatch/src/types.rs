913a5993b213b86961a48cddb37e358ad8a1cbac
Conor Patrick <conorpp94@gmail.com>
2021-04-19 00:22:43 -0700

    Move size definitions for large buffers to apdu-dispatch

--- components/apdu-dispatch/src/types.rs
+++ components/apdu-dispatch/src/types.rs
@@ -1,9 +1,63 @@
 
 
-interchange::interchange! {
-    ContactInterchange: (iso7816::command::Data, iso7816::response::Data)
+
+#[allow(non_camel_case_types)]
+type U6144 = <
+    heapless::consts::U4096 as core::ops::Add<heapless::consts::U2048>
+ >::Output;
+
+type U7168 = <
+    U6144 as core::ops::Add<heapless::consts::U1024>
+ >::Output;
+
+pub type U7609 = <
+    U7168 as core::ops::Add<heapless::consts::U441>
+ >::Output;
+
+
+#[allow(non_camel_case_types)]
+type U3072 = <
+    heapless::consts::U2048 as core::ops::Add<heapless::consts::U1024>
+ >::Output;
+
+// The max size that APDU dispatch will buffer chained commands up to.
+ #[allow(non_camel_case_types)]
+pub(crate) type LARGE_APDU_SIZE = U7609;
+
+// The max size that will be "transmitted" through the interchanges
+ #[allow(non_camel_case_types)]
+pub(crate) type MEDIUM_APDU_SIZE = U3072;
+
+
+pub mod command {
+    use super::*;
+    pub type Size = LARGE_APDU_SIZE;
+    pub type Data = iso7816::Bytes<Size>;
 }
 
-interchange::interchange! {
-    ContactlessInterchange: (iso7816::command::Data, iso7816::response::Data)
+pub mod response {
+    use super::*;
+    pub type Size = LARGE_APDU_SIZE;
+    pub type Data = iso7816::Bytes<Size>;
 }
+
+pub mod interchanges {
+    use super::*;
+    pub type Size = MEDIUM_APDU_SIZE;
+    pub type Data = iso7816::Bytes<Size>;
+
+    interchange::interchange! {
+        Contact: (Data, Data)
+    }
+
+    interchange::interchange! {
+        Contactless: (Data, Data)
+    }
+}
+
+
+// What apps can expect to send and recieve.
+pub type Command = iso7816::Command<command::Size>;
+pub type Response = iso7816::Response<response::Size>;
+
+
--- components/iso7816/src/lib.rs
+++ components/iso7816/src/lib.rs
@@ -3,11 +3,6 @@
 
 pub use heapless_bytes::Bytes as Bytes;
 
-#[allow(non_camel_case_types)]
-pub type U3072 = <heapless::consts::U2048 as core::ops::Add<heapless::consts::U1024>>::Output;
-#[allow(non_camel_case_types)]
-pub type MAX_COMMAND_DATA = U3072;
-
 // 7816-4, 8.2.1.2
 pub type Aid = Bytes<heapless::consts::U16>;
 
