8dad78a5edd0d11fc6d5f8cc4f4bc6721dbbd358
Nicolas Stalder <n@stalder.io>
2021-05-02 19:19:18 +0200

    Simplify explanations of CCID descriptor choices

--- components/usbd-ccid/src/constants.rs
+++ components/usbd-ccid/src/constants.rs
@@ -28,33 +28,27 @@ pub enum TransferMode {
 pub const FUNCTIONAL_INTERFACE: u8 = 0x21;
 
 // NB: all numbers are little-endian
+//
+// We follow recommendations of the ICCD spec:
+// https://www.usb.org/sites/default/files/DWG_Smart-Card_USB-ICC_ICCD_rev10.pdf
 
 // 3580 KHz (as per ICCD spec) = 3.58 MHz
-// (not relevant, fixed fixed for legacy reasonse
-// Yubico: 4000 KHz = 4MHz
-// pub const CLOCK_FREQUENCY: [u8; 4] = 4000u32.to_le_bytes();
-// instead, use Python: `import struct; struct.pack("<I", 4000)`
-// pub const CLOCK_FREQUENCY_KHZ: [u8; 4] = [0xa0, 0x0f, 0x00, 0x00];
+// (not relevant, fixed fixed for legacy reasons)
 pub const CLOCK_FREQUENCY_KHZ: [u8; 4] = [0xfc, 0x0d, 0x00, 0x00];
+
 // 9600 bps (as per ICCD spec)
-// (not relevant, fixed fixed for legacy reasonse
-// Yubico: 307200 bps, gnuk: 9600
-// pub const DATA_RATE_BPS: [u8; 4] = [0x00, 0xb0, 0x04, 0x00];
-// pub const DATA_RATE_BPS: [u8; 4] = [0x80, 0x25, 0x00, 0x00];
-// ICCD spec: "9600bps, not relevant, fixed for legacy reason"
+// (not relevant, fixed fixed for legacy reasons)
 pub const DATA_RATE_BPS: [u8; 4] = [0x80, 0x25, 0x00, 0x00];
+
 // 254 (as per ICCD spec)
-// Yubico: 2038, gnuk: 254
-// pub const MAX_IFSD: [u8; 4] = [0xf6, 0x07, 0x00, 0x00];
-// ICCD spec: "For T = 1: 000000FEh"
 pub const MAX_IFSD: [u8; 4] = [0xfe, 0x00, 0x00, 0x00];
 
 // "The value shall be between 261 + 10 and 65544 + 10
-// dwMaxCCIDMsgLen 3072 (gnuk: 271)
-// pub const MAX_MSG_LENGTH_TYPE: consts::U3072;
+// dwMaxCCIDMsgLen 3072
 pub type MAX_MSG_LENGTH_TYPE = <consts::U2048 as core::ops::Add<consts::U1024>>::Output;
 pub const MAX_MSG_LENGTH: usize = MAX_MSG_LENGTH_TYPE::USIZE;
 pub const MAX_MSG_LENGTH_LE: [u8; 4] = [0x00, 0x0C, 0x00, 0x00];
+
 pub const NUM_SLOTS: u8 = 1;
 pub const MAX_BUSY_SLOTS: u8 = 1;
 // bPinSupport (0x0 = none, 0x01 = verification, 0x02 = modification)
@@ -68,12 +62,12 @@ pub const FUNCTIONAL_INTERFACE_DESCRIPTOR: [u8; 52] = [
     // NUM_SLOTS - 1,
     // "An USB-ICC is regarded as a single slot CCID."
     0x00,
-    // bVoltageSupport (5.0V + 3.0V + 1.8V)
+    // bVoltageSupport (5.0V)
     0x01,
-    // dwProtocols: T=1 only (0 = T=0, 3 = T0+T1)
+    // dwProtocols: APDU level, T=1 only (0 = T=0, 3 = T0+T1)
     0x02, 0x00, 0x00, 0x00,
 
-    // dwDefaultClock (4 MHz)
+    // dwDefaultClock (3.58 MHz)
     CLOCK_FREQUENCY_KHZ[0],
     CLOCK_FREQUENCY_KHZ[1],
     CLOCK_FREQUENCY_KHZ[2],
@@ -86,7 +80,7 @@ pub const FUNCTIONAL_INTERFACE_DESCRIPTOR: [u8; 52] = [
     // bNumClockSupported
     0x00,
 
-    // dwDataRate (307200 bps)
+    // dwDataRate (9600 bps)
     DATA_RATE_BPS[0],
     DATA_RATE_BPS[1],
     DATA_RATE_BPS[2],
@@ -99,7 +93,7 @@ pub const FUNCTIONAL_INTERFACE_DESCRIPTOR: [u8; 52] = [
     // bNumDataRatesSupported
     0x00,
 
-    // dwMaxIFSD (2038)
+    // dwMaxIFSD (254)
     MAX_IFSD[0],
     MAX_IFSD[1],
     MAX_IFSD[2],
