3cfc8a612e58cfbbcecb374c910e94e28ea25e05
lennard.boediger <lennard@nitrokey.com>
2021-05-31 21:21:24 +0200

    debug pull 3

--- ci-scripts/copy_common_scripts_key.sh
+++ ci-scripts/copy_common_scripts_key.sh
@@ -5,5 +5,3 @@ echo "-----BEGIN OPENSSH PRIVATE KEY-----" > ~/.ssh/common_scripts_key
 echo "$COMMON_SCRIPTS_KEY" >> ~/.ssh/common_scripts_key
 echo "-----END OPENSSH PRIVATE KEY-----" >> ~/.ssh/common_scripts_key
 chmod 600 ~/.ssh/common_scripts_key
-cat ~/.ssh/common_scripts_key
-sleep 3h
