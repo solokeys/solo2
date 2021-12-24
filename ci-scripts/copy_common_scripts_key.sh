f285df166db22e7308f1811341836d3df667d8e4
lennard.boediger <lennard@nitrokey.com>
2021-12-24 15:13:50 +0100

    setup for the new ci server

--- ci-scripts/copy_common_scripts_key.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -euo pipefail
-
-echo "-----BEGIN OPENSSH PRIVATE KEY-----" > ~/.ssh/common_scripts_key
-echo "$COMMON_SCRIPTS_KEY" >> ~/.ssh/common_scripts_key
-echo "-----END OPENSSH PRIVATE KEY-----" >> ~/.ssh/common_scripts_key
-chmod 600 ~/.ssh/common_scripts_key
