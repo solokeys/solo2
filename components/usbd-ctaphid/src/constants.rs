d94be99355047c9ece8a2127992eb8ecf5ae6151
Nicolas Stalder <n@stalder.io>
2022-03-05 22:04:20 +0100

    Bump dependencies to published versions

--- components/usbd-ctaphid/src/constants.rs
+++ components/usbd-ctaphid/src/constants.rs
@@ -3,5 +3,5 @@ pub const INTERRUPT_POLL_MILLISECONDS: u8 = 5;
 
 pub const PACKET_SIZE: usize = 64;
 
-// 7609 bytes
-pub const MESSAGE_SIZE: usize = PACKET_SIZE - 7 + 128 * (PACKET_SIZE - 5);
+// 1200
+pub const MESSAGE_SIZE: usize = ctap_types::sizes::REALISTIC_MAX_MESSAGE_SIZE;
