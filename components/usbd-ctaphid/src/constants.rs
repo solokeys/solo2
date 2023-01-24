f3f4faa8e2a446de3e91bf51c5cd0c71d0b0ff45
Nicolas Stalder <n@stalder.io>
2023-01-24 23:05:33 +0100

    Migrate

--- components/usbd-ctaphid/src/constants.rs
+++
@@ -1,8 +0,0 @@
-
-pub const INTERRUPT_POLL_MILLISECONDS: u8 = 5;
-
-pub const PACKET_SIZE: usize = 64;
-
-// 1200
-// pub const MESSAGE_SIZE: usize = ctap_types::sizes::REALISTIC_MAX_MESSAGE_SIZE;
-pub const MESSAGE_SIZE: usize = 3072;
